ad8012e82dbec836a1d8d8d38102c78975e43cb9
unknown <henrytesting8@gmail.com>
2023-04-05 22:04:39 +0700

    templeting engine .pug l-84

--- app.js
+++ app.js
@@ -6,8 +6,8 @@ const bodyParser = require("body-parser");
 const app = express();
 
 // using templete engine
-app.set('view engine','pug')
-app.set('views','views')
+app.set("view engine", "pug");
+app.set("views", "views");
 
 const adminRoutes = require("./routes/admin");
 const shopRoutes = require("./routes/shop");
@@ -19,7 +19,8 @@ app.use("/admin", adminRoutes.routes);
 app.use(shopRoutes);
 
 app.use((req, res, next) => {
-  res.status(404).sendFile(path.join(__dirname, "views", "404.html"));
+  // res.status(404).sendFile(path.join(__dirname, "views", "404.html"));
+  res.status(404).render("404");
 });
 
 app.listen(3000);
--- routes/admin.js
+++ routes/admin.js
@@ -10,13 +10,14 @@ const products = [];
 
 // /admin/add-product => GET
 router.get("/add-product", (req, res, next) => {
-  res.sendFile(path.join(rootDir, "views", "add-product.html"));
+  // res.sendFile(path.join(rootDir, "views", "add-product.html"));
+  res.render("add-product", { pageTitle: "Add Product" });
 });
 
 // /admin/add-product => POST
 router.post("/add-product", (req, res, next) => {
   products.push({ title: req.body.title });
- // console.log("admin.js = ", products);
+  // console.log("admin.js = ", products);
   res.redirect("/");
 });
 
