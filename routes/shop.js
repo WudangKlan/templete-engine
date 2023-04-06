192b4c320bf5b491fe9516d99550bef88dbd9291
Henry.Yeo <Henry.Yeo@easecoxgroup.com>
2023-04-06 17:41:24 +0700

    completed pug l-86

--- routes/shop.js
+++ routes/shop.js
@@ -14,7 +14,7 @@ router.get("/", (req, res, next) => {
 
   //retun templete engine
   const products = adminData.products;
-  res.render("shop", { prods: products, docTitle: "Shop" });
+  res.render("shop", { prods: products, pageTitle: "Shop" , path:"/"});
 });
 
 module.exports = router;
